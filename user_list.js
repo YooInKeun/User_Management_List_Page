a65c227e73b6a404bc5bc96220165333015d0f52
YooInKeun <keun0390@naver.com>
2020-01-07 17:09:37 +0900

    [ADD] 기본 페이지 레이아웃 및 기본 통신 완료

---
+++ user_list.js
@@ -0,0 +1,26 @@
+axios.defaults.baseURL = 'http://localhost:8580/';
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        users: [],
+    },
+    created: function () {
+        this.fetch_all_todo();
+    },
+    methods: {
+        fetch_all_todo: function () {
+            var vm = this;
+            var tmpCondition = '?age=&end_dt=&pageCon=10&pageNum=1&start_dt=&tel=&userSortDirection=ASC&userSortItem=user_seq&user_id=&user_name=&view_cnt=';
+            
+            axios.get('v1/chunjae/user/selectUser/' + tmpCondition)
+                .then(function (res) {
+                    console.log("GET RES", res);
+                    vm.users = res.data.data.rows;
+                })
+                .catch(function (err) {
+                    console.log("GET ERR", err);
+                })
+        },
+    },
+})
