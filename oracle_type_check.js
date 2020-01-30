96cf26e775dd76fe3d071065cbb0be023a08c3c1
YooInKeun <keun0390@naver.com>
2020-01-30 17:36:07 +0900

    [ADD] Oracle Data Type Mapping Test

---
+++ oracle_type_check.js
@@ -0,0 +1,35 @@
+axios.defaults.baseURL = 'http://localhost:8580/';
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        result: {}
+     },
+    created: function () {
+        this.fetch_all_data();
+    },
+    methods: {
+        fetch_all_data: function () {
+
+            var json = {
+                params: {
+                    col_int: 239
+                }
+            };
+
+            axios.get('/chunjae/typecheck/oracle', json)
+                .then((res) => {
+                    console.log("GET SUCESS", res);
+                    result = JSON.parse(res.data.data);
+                    console.log(result);
+
+                    for (key in result) {
+                        console.log('key : ' + key + ' / ' + 'type : ' + typeof(result[key]) + ' / ' + 'value:' + result[key]);
+                    };
+                })
+                .catch((err) => {
+                    console.log("GET FAIL", err);
+                });
+        },
+    },
+})
